6e4d4273710e57db530c04d55b7a2008523d6dad
Gabriel Navarro <gbnavarro@usp.br>
2024-10-08 16:26:38 -0300

    Update FowardCar.java

--- FowardCar.java
+++ FowardCar.java
@@ -9,13 +9,15 @@ class AvoidObstacle implements Behavior {
 		sonar = new UltrasonicSensor(SensorPort.S3); 
 	} 
 	
-public boolean takeControl() { 
+	public boolean takeControl() { 
 		return sonar.getDistance() < 25 ;  //returns True if an obstacle is detected
 	} 
+	
 	public void suppress() { 
 		//Since this is the highest priority behavior, 
 		// suppress will never be called. 
 	} 
+	
 	public void action() { 
 		LCD.clear(); 
 		LCD.drawString("DetectWall",0,1); 	
@@ -26,13 +28,16 @@ public boolean takeControl() {
 
 class DriveForward implements Behavior { 
 	private boolean suppressed = false; 
+	
 	public boolean takeControl() { 
 		return true; // this behavior always wants control, no matter the situation
 	}
+	
 	public void suppress() { 
 		suppressed = true; // standard practice (some behaviors can do something 
                                             // else before stopping)
 	} 
+	
 	public void action() { 
 		LCD.clear(); 
 		LCD.drawString("DriveForward",0,1); 
@@ -52,10 +57,11 @@ public class ForwardCar {
   // renaming  instances of Motor.A and Motor.B de ...
 	static NXTRegulatedMotor leftMotor = Motor.A; 
 	static NXTRegulatedMotor rightMotor = Motor.B; 
+	
 	public static void main(String[] args) { 
 		leftMotor.setSpeed(300); 
 		rightMotor.setSpeed(300); 
-             // create objects  b1 and b2 
+             	// create objects  b1 and b2 
 		Behavior b1 = new DriveForward(); 
 		Behavior b2 = new AvoidObstacle(); 
 		// create a list of behaviors: the order indicates the priority
